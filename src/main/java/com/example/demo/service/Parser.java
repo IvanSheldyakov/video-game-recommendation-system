b99c017050128177ac368693611520ac39fa0464
Ivan Sheldyakov <i.sheldyakov@g.nsu.ru>
2023-05-30 14:54:10 +0700

    add block words

--- src/main/java/com/example/demo/service/Parser.java
+++ src/main/java/com/example/demo/service/Parser.java
@@ -9,13 +9,14 @@ import org.springframework.stereotype.Component;
 public class Parser {
 
     private final BeanFactory beanFactory;
+    private final KeywordsService keywordsService;
 
     public void parse() {
         try {
             for (int i = 5; i <= 9; i++) { //198
                 PageParser parser = beanFactory.getBean(PageParser.class, i);
                 parser.parse();
-
+                //keywordsService.update(); каждые 1000 игр
             }
             System.out.println("done");
 
