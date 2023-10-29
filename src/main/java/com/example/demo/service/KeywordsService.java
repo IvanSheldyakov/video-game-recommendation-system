9a3ba73cb0850c1b7307422caa7fe84ca7192a06
Иван Павлович Шелдяков <i.sheldyakov@g.nsu.ru>
2023-10-30 00:13:31 +0700

    clean

--- src/main/java/com/example/demo/service/KeywordsService.java
+++ src/main/java/com/example/demo/service/KeywordsService.java
@@ -6,7 +6,6 @@ import com.example.demo.repository.TypeRepository;
 import com.example.demo.repository.WordCountRepository;
 import com.example.demo.repository.WordRepository;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -24,13 +23,7 @@ public class KeywordsService {
   private final WordRepository wordRepository;
 
   private final BlockWordRepository blockWordRepository;
-
-  // public static double k = 1.5;
-
   private boolean needBlock = false;
-
-  private final Comparator<WordCount> wordCountComparator =
-      Comparator.comparing(WordCount::getCount);
 
   @Transactional
   public void update() {
@@ -48,7 +41,9 @@ public class KeywordsService {
     }
     Set<String> blockWords =
         blockWordRepository.findAll().stream().map(BlockWord::getWord).collect(Collectors.toSet());
+
     List<Type> types = typeRepository.findAll();
+
     Set<String> allKeywords =
         types.stream()
             .flatMap(type -> wordRepository.findWordsByType(type).stream().map(Word::getWord))
@@ -60,14 +55,13 @@ public class KeywordsService {
           List<WordCount> countList =
               wordCountRepository.findAllByTypeNameOrderByCountAsc(type.getTypeName());
           double gameCount = type.getGames().size() * 0.7;
+
           if (countList.isEmpty()) return;
 
           List<WordCount> list =
               countList.stream().filter(wordCount -> wordCount.getInGames() > gameCount).toList();
 
           Bounds bounds = findBounds(list);
-          //  System.out.println(bounds +
-          // "---------------------------------------------------------------------------------------------------------------------");
 
           list.stream()
               .filter(wordCount -> bounds.inBounds(wordCount.getCount()))
@@ -85,31 +79,10 @@ public class KeywordsService {
     System.out.println(newKeyWords.size());
   }
 
-  private Double findMedian(List<WordCount> countList) {
-    double median;
-    int size = countList.size();
-    if (size % 2 == 0) {
-      // Если размер списка четный, возвращаем среднее значение двух средних элементов
-      long leftMiddle = countList.get(size / 2 - 1).getCount();
-      long rightMiddle = countList.get(size / 2).getCount();
-      return median = (leftMiddle + rightMiddle) / 2.0;
-    } else {
-      // Если размер списка нечетный, возвращаем значение среднего элемента
-      return median = Long.valueOf(countList.get(size / 2).getCount()).doubleValue();
-    }
-  }
-
   private Bounds findBounds(List<WordCount> countList) {
     Double Q1 = findQ1(countList);
     Double Q3 = findQ3(countList);
-    Double IQR = Q3 - Q1;
-    // Double lower = Q1 - (IQR * k);
-    // Double upper = Q3 + (IQR * k);
     return new Bounds(Q1, Q3);
-  }
-
-  private Double findIQR(List<WordCount> countList) {
-    return findQ3(countList) - findQ1(countList);
   }
 
   private Double findQ1(List<WordCount> countList) {
